17674db767346dd1c4e113aad67d963e36b8be7b
matsribeiro <matheus.avelar.1265737@sga.pucminas.br>
2022-02-24 09:17:22 -0300

    push

--- tps/tp01/TP01Q04 - Alteração Aleatória em Java/TP01Q04.java
+++ tps/tp01/TP01Q04 - Alteração Aleatória em Java/TP01Q04.java
@@ -2,21 +2,20 @@ import java.util.Random;
 import java.lang.Math;
 
 class TP01Q04{
-    
     private static Random gerador = new Random();
 
-    //FIM
+    //FIM (reciclagem)
     public static boolean isFim(String s){
         return(s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
     }
 
-    //Altera os caracteres da string "s" com a constante do gerador
-    public static String aleatorio(String s){
+    //Altera os caracteres da string "s" com a constante do gerador (4)
+    public static String alteracaoAleatorio(String s){
         String resp = "";
         char char_s, char_resp;
 
-        char_s = (char)(Math.abs(gerador.nextInt()) % 26 + 'a');
-        char_resp = (char)(Math.abs(gerador.nextInt()) % 26 + 'a');
+        char_s = (char)('a' + Math.abs(gerador.nextInt()) % 26);
+        char_resp = (char)('a' + Math.abs(gerador.nextInt()) % 26);
 
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == char_s){
@@ -31,6 +30,7 @@ class TP01Q04{
 
     //Main (reciclagem)
     public static void main(String[] args){
+        //Seed = 4
         gerador.setSeed(4);
         String[] entrada = new String[1000];
         int num_linha = 0;
@@ -41,7 +41,7 @@ class TP01Q04{
         num_linha--;
 
         for(int i = 0; i < num_linha; i++){
-            MyIO.println(aleatorio(entrada[i]));
+            MyIO.println(alteracaoAleatorio(entrada[i]));
         }
     }
 }
